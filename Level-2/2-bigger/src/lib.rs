86520da608889c1ed0ade5edc1838d84e413c271
cisall <ciresall79@gmail.com>
2024-05-31 23:28:43 +0000

    First_version Level2

--- Level-2/2-bigger/src/lib.rs
+++ Level-2/2-bigger/src/lib.rs
@@ -1,14 +1,5 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
-}
+use std::collections::HashMap;
 
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
+pub fn bigger(h: HashMap<&str, i32>) -> i32 {
+    h.values().filter(|&&v| v > 0).cloned().max().unwrap_or(0)
 }
--- Level-2/2-capitalizing/src/lib.rs
+++ Level-2/2-capitalizing/src/lib.rs
@@ -1,14 +1,29 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+pub fn capitalize_first(input: &str) -> String {
+    if let Some(first) = input.chars().next() {
+        let mut capitalized = first.to_uppercase().to_string();
+        capitalized.push_str(&input[1..]);
+        capitalized
+    } else {
+        String::new()
+    }
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+pub fn title_case(input: &str) -> String {
+    input
+        .split_whitespace()
+        .map(capitalize_first)
+        .collect::<Vec<_>>()
+        .join(" ")
+}
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+pub fn change_case (input: &str) -> String{
+    let mut result = String::new();
+    for (i, c) in input.chars().enumerate() {
+        if i % 2 == 0 {
+            result.push_str(&c.to_uppercase().to_string());
+        } else {
+            result.push_str(&c.to_lowercase().to_string());
+        }
     }
+    result
 }
