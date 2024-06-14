236bdaee9b8980d5cbbebe020a14fce1c6a36202
cisall <ciresall79@gmail.com>
2024-06-14 23:26:15 +0000

    update

--- Exam-3/Level-3/rpn/src/lib.rs
+++ Exam-3/Level-3/rpn/src/lib.rs
@@ -1,14 +1,79 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
-}
+use std::env;
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+pub fn rpn(input: &str) {
+    let mut stack = Vec::new();
+    let tokens = input.split_whitespace();
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+    for token in tokens {
+        match token {
+            "+" => {
+                if stack.len() < 2 {
+                    println!("Error");
+                    return;
+                }
+                let b = stack.pop().unwrap();
+                let a = stack.pop().unwrap();
+                stack.push(a + b);
+            },
+            "-" => {
+                if stack.len() < 2 {
+                    println!("Error");
+                    return;
+                }
+                let b = stack.pop().unwrap();
+                let a = stack.pop().unwrap();
+                stack.push(a - b);
+            },
+            "*" => {
+                if stack.len() < 2 {
+                    println!("Error");
+                    return;
+                }
+                let b = stack.pop().unwrap();
+                let a = stack.pop().unwrap();
+                stack.push(a * b);
+            },
+            "/" => {
+                if stack.len() < 2 {
+                    println!("Error");
+                    return;
+                }
+                let b = stack.pop().unwrap();
+                let a = stack.pop().unwrap();
+                if b == 0 {
+                    println!("Error");
+                    return;
+                }
+                stack.push(a / b);
+            },
+            "%" => {
+                if stack.len() < 2 {
+                    println!("Error");
+                    return;
+                }
+                let b = stack.pop().unwrap();
+                let a = stack.pop().unwrap();
+                if b == 0 {
+                    println!("Error");
+                    return;
+                }
+                stack.push(a % b);
+            },
+            _ => {
+                match token.parse::<i64>() {
+                    Ok(num) => stack.push(num),
+                    Err(_) => {
+                        println!("Error");
+                        return;
+                    },
+                }
+            }
+        }
+    }
+
+    if stack.len() == 1 {
+        println!("{}", stack.pop().unwrap());
+    } else {
+        println!("Error");
     }
 }
